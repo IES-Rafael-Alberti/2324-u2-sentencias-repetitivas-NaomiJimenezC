25b8c08f3d64c1a044cdeb9b15e883c0770866f0
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-17 17:59:49 +0200

    correcion test ejercicio1

--- test/test_ejercicio1.py
+++ test/test_ejercicio1.py
@@ -1,6 +1,5 @@
 from src.ejercicio1 import repetir_diez_veces
 
 def test_repetision_palabra():
-    assert len(repetir_diez_veces("patata")) == 4
     assert len(repetir_diez_veces("patata")) == 10
     
