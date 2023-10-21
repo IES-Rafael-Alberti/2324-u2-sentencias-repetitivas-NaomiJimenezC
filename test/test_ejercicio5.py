70e6ead1b2ac83bfdff6f757637ead643637a942
IgnacioJimenezC <ijimenez@iesrafaelalberti.es>
2023-10-22 00:30:15 +0200

    ejercicio 5 hecho

--- test/test_ejercicio5.py
+++ test/test_ejercicio5.py
@@ -4,9 +4,8 @@ from src.ejercicio5 import *
 def test_comprobar_porcentajes():
     assert convertir_porcentaje_a_float("95%") == 0.95
     assert convertir_porcentaje_a_float("0%") == 0.0
-    assert convertir_porcentaje_a_float("46%") == 0.95
     assert convertir_porcentaje_a_float("33%") == 0.33
     assert convertir_porcentaje_a_float("7%") == 0.07
 
 def test_calcular_interes_anual():
-    assert calcular_interes_anual(15000,"25%") == 
+    assert calcular_interes_anual(15000,"25%") == 18750
